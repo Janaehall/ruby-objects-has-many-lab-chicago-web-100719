e6032d6421c87cdc54f3492e8b194fd57a09fe4c
Janae Hall <janaeelisa@gmail.com>
2019-10-13 05:07:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,19 @@
 require_relative 'artist'
 class Song
 
+  @@all = []
+
   attr_reader :name
   attr_accessor :artist
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def artist=(artist)
     @artist = artist
     artist.songs << self
   end
-
 
 end

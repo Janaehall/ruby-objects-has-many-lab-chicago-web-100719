b83dc031ea0af37a5f2de97180f51b786c8eb9b0
Janae Hall <janaeelisa@gmail.com>
2019-10-13 05:08:59 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+require 'pry'
+require_relative 'song'
+
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+
+  def self.song_count
+    @@all.sum {|a| a.songs.length}
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+require_relative 'artist'
+class Song
+
+  @@all = []
+
+  attr_reader :name
+  attr_accessor :artist
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.songs << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    end
+  end
+
+end

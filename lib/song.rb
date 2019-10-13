8cdd17a4cb106e0517620fab5b80995ad27b9f42
Janae Hall <janaeelisa@gmail.com>
2019-10-13 04:52:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,17 @@
 require_relative 'artist'
 class Song
 
-  attr_reader :title
+  attr_reader :name
   attr_accessor :artist
 
-  def initialize(title)
-    @title = title
+  def initialize(name)
+    @name = name
   end
 
   def artist=(artist)
     @artist = artist
     artist.songs << self
   end
+  
 
 end

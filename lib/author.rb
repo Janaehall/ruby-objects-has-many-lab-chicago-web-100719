ee4f5a87c2ac7d9f47643d576a492ecc634ccb46
Janae Hall <janaeelisa@gmail.com>
2019-10-13 05:04:05 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,12 @@
 class Author
   attr_reader :name, :posts
 
+  @@all = []
+
   def initialize(name)
     @name = name
     @posts = []
+    @@all << self
   end
 
   def add_post(post)
@@ -13,6 +16,10 @@ class Author
   def add_post_by_title(title)
     post = Post.new(title)
     self.add_post(post)
+  end
+
+  def post_count
+    @@all.sum {|a| a.posts}
   end
 
 end

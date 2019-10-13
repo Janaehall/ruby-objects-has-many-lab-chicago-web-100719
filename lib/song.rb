543ff33091631d367026bf71c76a219c50739378
Janae Hall <janaeelisa@gmail.com>
2019-10-13 04:43:53 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+require_relative 'artist'
+class Song
+
+  attr_reader :title
+  attr_accessor :artist
+
+  def initialize(title)
+    @title = title
+  end
+
+  def artist=(artist)
+    artist.songs << self
+  end
+
+end
---
+++ lib/test.rb
@@ -0,0 +1,8 @@
+require_relative 'artist'
+require_relative 'song'
+require 'pry'
+
+adele = Artist.new("Adele")
+rolling = Song.new("Rolling in the Deep")
+
+binding.pry

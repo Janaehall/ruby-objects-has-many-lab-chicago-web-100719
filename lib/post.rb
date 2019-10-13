ab91b6f6dfd51b64348123ebace14d8ccf288828
Janae Hall <janaeelisa@gmail.com>
2019-10-13 05:01:01 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,12 @@
 class Post
-
+  attr_reader :content, :author
   def initialize(content)
     @content = content
   end
+
+  def author=(author)
+    @author = author
+    author.posts << self
+  end
+
 end
